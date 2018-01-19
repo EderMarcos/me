e39dc270b214557cbb6c7bfc2434789ca34d191c
EderMarcos <skrap.marcos@gmail.com>
2018-01-19 00:55:11 -0600

    Version beta

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,31 @@ $(function () {
     $('.card-columns .card').on('click', function () {
         $('#modal-content').modal();
         var path = ($(this).find('img').attr('src'));
-        console.log(path);
         $('#img-modal-content').attr('src', path);
     });
     $('#modal-content').on('click', function () {
         $('#modal-content').modal('hide');
     });
+
+    var nav_links = document.querySelectorAll('.nav-link');
+    var scrollPosition = 0;
+    for (var i = 0; i < nav_links.length; i++) {
+        nav_links[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            var targetPosition = document.querySelector(e.target.getAttribute('href')).offsetTop;
+
+            var interval = setInterval(function () {
+                if(scrollPosition < targetPosition) {
+                    scrollPosition += 100;
+                    if(scrollPosition >= targetPosition) {
+                        scrollPosition = targetPosition;
+                        clearInterval(interval);
+                    }
+                }
+                window.scrollTo(0, scrollPosition);
+            }, 25);
+
+
+        })
+    }
 });
